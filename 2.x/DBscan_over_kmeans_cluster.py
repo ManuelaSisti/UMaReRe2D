75d32cd94d8606220ca265aba1fe2c81506da6a8
ManuelaSisti <65607117+ManuelaSisti@users.noreply.github.com>
2020-11-18 16:26:02 +0100

    Add files via upload

---
+++ 2.x/DBscan_over_kmeans_cluster.py
@@ -0,0 +1,122 @@
+import h2o 
+from h2o.estimators import H2OIsolationForestEstimator
+from sklearn.cluster import DBSCAN
+import pandas as pd
+import numpy as np
+
+h2o.init()
+
+#reading file using pandas
+h2o_df = h2o.import_file(file_kmeans)
+if 'label' in h2o_df.columns:
+  h2o_df_work = h2o_df[h2o_df['label']==label_rec]
+  h2o_df_work = h2o_df_work.drop(['label'])
+else:
+  h2o_df_work = h2o_df.loc[:]
+
+coord_sel = h2o_df_work.drop([0,3,4,5,6,7,8], axis=1)
+
+#applying dbscan 
+coord_array = coord_sel.as_data_frame().as_matrix()
+clustering = DBSCAN(eps=50, min_samples=100).fit(coord_array)
+clustering.labels_
+
+#converting again to h2o data frame
+to_pandas = pd.DataFrame(clustering.labels_)
+clusterized = h2o.H2OFrame(to_pandas, column_names=['clusterization'])
+h2o_df_work = h2o_df_work.cbind(clusterized)
+
+#pandas
+h2o_to_pandas = h2o_df_work.as_data_frame()
+
+#saving
+np.savetxt(file_DBscan,h2o_to_pandas.values, header = 'J\tx\ty\tV\tEV\tEVB\tCM\tJep\tclusterization',fmt='%f %d %d %f %f %f %f %f %d')
+
+#==========================================================================================
+#in this second part we apply periodicity to the obtained clusters, if the box is periodic
+
+#reading clusters obtained applying DBscan 
+test_mask = np.zeros((nx,ny,2), dtype=int)
+file1 = open(file_DBscan, 'r')
+file_DBscan_length = len(file1.readlines())
+file1.seek(0)
+file1.readline()
+file1.readline().split()
+for i in range(0,file_DBscan_length-2):
+    tr = file1.readline().split()
+    a = int(tr[1])
+    b = int(tr[2])
+    test_mask[a,b,0] = 1
+    test_mask[a,b,1] = int(tr[8])+1 #+1 is necessary in order to distinguish clusters from the base value (-1 indicates isolated points)
+
+file1.close()
+
+#Merging clusters using periodicity
+for ix in range(0,len(test_mask[:,0,1])):
+    if (test_mask[ix,0,1] != 0):
+        if (test_mask[ix,-1,1] != 0):
+             xx, yy = np.where(test_mask[:,:,1]==test_mask[ix,-1,1]) 
+             test_mask[xx,yy,1] = test_mask[ix,0,1]
+
+#Merging current sheets using periodicity
+for iy in range(0,len(test_mask[0,:,1])):
+    if (test_mask[0,iy,1] != 0):
+        if (test_mask[-1,iy,1] != 0):
+             xx, yy = np.where(test_mask[:,:,1]==test_mask[-1,iy,1]) 
+             test_mask[xx,yy,1] = test_mask[0,iy,1]
+
+mmm = np.amax(test_mask[:,:,1])
+#print(mmm)
+
+real_cluster_number = 0
+for i in range(1,mmm+1):
+    xxx,yyy = np.where(test_mask[:,:,1]==i)
+    if (np.shape(xxx)[0] !=0):
+        real_cluster_number = real_cluster_number + 1
+        test_mask[xxx,yyy,1] = real_cluster_number
+
+#-------------------------------------------------------------------------------------
+#figure test
+plt.figure(figsize=(10,8.5))
+plt.contourf(Jn[:,:,0].T,30)
+plt.colorbar()
+for i in range(1,np.amax(test_mask[:,:,1])+1):
+    xindex, yindex = np.where(test_mask[:,:,1]==i)
+    plt.plot(xindex,yindex,'.',markersize=1)
+
+plt.title('Clusters (K-means + DBscan), t=%.f, over $|J|$' %time)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.tight_layout()
+plt.show(0)
+
+#-------------------------------------------------------------------------------------
+#saving positions in grid points of local maxima for each cluster
+#saving test_mask
+
+maxima_cluster = np.zeros((0))
+maxima_cluster_x = np.zeros((0))
+maxima_cluster_y = np.zeros((0))
+
+for i in range(1,np.amax(test_mask[:,:,1])+1):
+    xindex, yindex = np.where(test_mask[:,:,1]==i)
+    Jn_cluster = Jn[xindex,yindex,0]
+    maximum_cluster = np.amax(Jn_cluster) #WARNING: it is necessary to have uploaded data for the right time
+    maximum_cluster_location = np.where(Jn_cluster==maximum_cluster)
+    maxima_cluster_x = np.append(maxima_cluster_x,xindex[maximum_cluster_location])
+    maxima_cluster_y = np.append(maxima_cluster_y,yindex[maximum_cluster_location])
+    maxima_cluster = np.append(maxima_cluster,maximum_cluster)
+
+#writing maxima
+file1 = open(file_maxima, 'w')
+for i in range(0,len(maxima_cluster)):
+    tw1 = '%.f' % maxima_cluster_x[i]
+    tw2 = '%.f' % maxima_cluster_y[i]
+    file1.write(tw1 + '\t' + tw2 + '\n')
+
+file1.close()
+
+#writing test_mask
+np.savetxt(file_mask_binary,test_mask[:,:,0])
+np.savetxt(file_mask_clusters,test_mask[:,:,1])
+
---
+++ 2.x/clusters_aspect_ratio.py
@@ -0,0 +1,38 @@
+import utilities_unsup as ut
+
+#------------------------------------------------------
+#loading maxima and test_mask
+cluster_maxima_and_loc = np.zeros((real_cluster_number,2))
+
+file2 = open(file_maxima, 'r')
+for i in range(0,real_cluster_number):
+    tr = file2.readline().split()
+    cluster_maxima_and_loc[i,0] = float(tr[0])
+    cluster_maxima_and_loc[i,1] = float(tr[1])
+
+file2.close()
+
+test_mask = np.zeros((nx,ny,2), dtype=int)
+test_mask[:,:,0] = np.loadtxt(file_mask_binary)
+test_mask[:,:,1] = np.loadtxt(file_mask_clusters)
+
+#------------------------------------------------------
+#Computing width and thickness of each cluster
+
+print('Computing width (max distance method)')
+width = ut.structure_width(ut,real_cluster_number,test_mask[:,:,1],nx,ny,nz,dl,base_value=0)
+H, eigenvalues2D, eigenvectors2D = ut.Hmatrix(ut,Jn[:,:,:],real_cluster_number,cluster_maxima_and_loc[:,0],cluster_maxima_and_loc[:,1],nx,ny,nz,dl,normalization = True)
+Jn_per = np.tile(Jn[:,:,0],(2,2))
+print('Computing thickness (FWHM method)')
+FWHM_thickness = ut.interpolation_for_width_and_thickness(ut,eigenvalues2D,eigenvectors2D,real_cluster_number,cluster_maxima_and_loc[:,0],cluster_maxima_and_loc[:,1],dl,Jn_per,nx,ny,nz)
+
+#Computing aspect ratio
+print('Computing aspect ratio (width/thickness)')
+aspect_ratio = width/FWHM_thickness
+
+counter = 0
+for i in range(0,len(aspect_ratio)): #correction necessary when the cluster is almost circular and width~thickness
+    if (aspect_ratio[i] < 1.0):
+        aspect_ratio[i] = 1.0/aspect_ratio[i]
+        counter = counter + 1
+
